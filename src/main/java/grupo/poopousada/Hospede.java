0c64d7d0f690d030967b108bef0ac8efa98e4bfa
Nataniel Fonseca <nataniel.fonseca@aluno.ufca.edu.br>
2023-04-23 17:25:55 -0300

    agora o sistema lê e grava os dados de hospedes em um arquivo txt

--- src/main/java/grupo/poopousada/Hospede.java
+++ src/main/java/grupo/poopousada/Hospede.java
@@ -5,24 +5,37 @@ package grupo.poopousada;
  */
 public class Hospede {
     private String nome;
-    private String email;
+    private String cpf;
+    private String endereco;
     private String telefone;
+    
+    
 
-    public Hospede(String nome, String email, String telefone) {
+    public Hospede(String nome, String cpf, String endereco, String telefone) {
         this.nome = nome;
-        this.email = email;
+        //this.email = email;
         this.telefone = telefone;
+        this.endereco = endereco;
+        this.cpf = cpf;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public String getEmail() {
+    /*public String getEmail() {
         return email;
-    }
+    }*/
 
     public String getTelefone() {
         return telefone;
+    }
+    
+    public String getCpf() {
+        return cpf;
+    }
+    
+    public String getEndereco(){
+        return endereco;
     }
 }
